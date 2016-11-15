4fc82e8970c589c072998a757e3a08411d417647
EmilieBo <bonizecemilie@gmail.com>
2016-11-15 10:37:58 +0100

    ajout d'un bout de javadoc

--- Projet_BDT/src/wargame/IConfig.java
+++ Projet_BDT/src/wargame/IConfig.java
@@ -1,5 +1,10 @@
 package wargame;
 import java.awt.Color;
+/**
+ * 
+ * @author emilie
+ *
+ */
 public interface IConfig {
 	int LARGEUR_CARTE = 25; int HAUTEUR_CARTE = 15; // en nombre de cases
 	int NB_PIX_CASE = 20;
