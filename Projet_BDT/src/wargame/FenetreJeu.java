f937e419069c1d6db14d5775e89c550a5eb4773d
hugo <hugo@francis.home>
2016-11-16 21:47:55 +0100

    Modifications de FenetreJeu (à terminer y'a du boulot)

--- Projet_BDT/src/wargame/FenetreJeu.java
+++ Projet_BDT/src/wargame/FenetreJeu.java
@@ -3,40 +3,52 @@ package wargame;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import wargame.PanneauJeu;
 
-public class FenetreJeu implements IConfig{
-
+public class FenetreJeu  implements IConfig {
+	public static int abs=0;
+	public static int ord=0;
 	public static void main(String[] args){
 		
 		JFrame frame = new JFrame("Wargame");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
 		PanneauJeu panneauJeu = new PanneauJeu();
-		panneauJeu.setOpaque(true);
-		panneauJeu.setBackground(Color.GRAY); 
-		panneauJeu.setPreferredSize(new Dimension(LARGEUR_CARTE*NB_PIX_CASE,HAUTEUR_CARTE*NB_PIX_CASE));
-	
 		JPanel infosTop = new JPanel();
-		JLabel infosBot = new JLabel();
+		JLabel infosBot = new JLabel("vide");
 		
+		frame.addMouseMotionListener(new MouseAdapter(){
+			public void MouseMoved(MouseEvent e){
+				abs=e.getX();
+				ord=e.getY();
+			infosBot.setText("coord : "+abs+" " +ord);
+			System.out.println("ee");
+			}
+		})	;	
 		infosTop.setOpaque(true);
 		infosTop.setPreferredSize(new Dimension(LARGEUR_CARTE*NB_PIX_CASE,2*NB_PIX_CASE));
 		infosTop.setBackground(COULEUR_VIDE);
 		infosBot.setOpaque(true);
 		infosBot.setPreferredSize(new Dimension(LARGEUR_CARTE*NB_PIX_CASE,1*NB_PIX_CASE));
 		infosBot.setBackground(COULEUR_VIDE);
+		panneauJeu.setPreferredSize(new Dimension(LARGEUR_CARTE*NB_PIX_CASE+1,1+HAUTEUR_CARTE*NB_PIX_CASE));
+		frame.getContentPane().add(panneauJeu,BorderLayout.CENTER);
 
 		frame.getContentPane().add(infosTop,BorderLayout.NORTH);
-		frame.getContentPane().add(panneauJeu,BorderLayout.CENTER);
 		frame.getContentPane().add(infosBot,BorderLayout.SOUTH);
+
 
 		frame.setLocation(POSITION_X, POSITION_Y);
 		frame.pack();
 		frame.setVisible(true);
+		
+		
 	}
 }
