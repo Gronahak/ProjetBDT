0605c7076d6a92741fa47168d07b112c23b02c20
hugo <hugo@francis.home>
2016-11-16 22:55:52 +0100

    merge

--- Projet_BDT/src/wargame/ICarte.java
+++ Projet_BDT/src/wargame/ICarte.java
@@ -3,17 +3,84 @@ import java.awt.Graphics;
 /**
  * Interface pour la gestion de carte
  * @author Emilie, Hugo, Rémy
- * @version 11-11-2016
+ * @version 16/11/2016
  */
 public interface ICarte {
+	/** 
+	 * Récupère l'élément à la position {@code pos}.
+	 * @param pos La position de l'élément souhaité
+	 * @return l'élément sur la carte à cette position
+	 */
 	Element getElement(Position pos);
+<<<<<<< HEAD
 	Position trouvePositionVide(int zone); 				// Trouve aléatoirement une position vide sur la carte
 	Position trouvePositionVide(Position pos); 	// Trouve une position vide choisie aléatoirement parmi les 8 positions adjacentes de pos
 	Heros trouveHeros(); 						// Trouve aléatoirement un héros sur la carte
 	Heros trouveHeros(Position pos); 			// Trouve un héros choisi aléatoirement parmi les 8 positions adjacentes de pos
+=======
+	
+	/**
+	 * Trouve aléatoirement une position vide sur la carte
+	 * @return une position (case vide)
+	 */
+	Position trouvePositionVide();
+	
+	/**
+	 * Trouve aléatoirement une position vide sur la carte parmi les cases adjacentes (8) à {@code pos}.
+	 * @param pos La position à partir de laquelle on recherche une position aléatoire
+	 * @return une position (case vide)
+	 */
+	Position trouvePositionVide(Position pos);
+	
+	/**
+	 * Trouve aléatoirement un héros sur la carte
+	 * @return un héros
+	 */
+	Heros trouveHeros();
+	
+	/**
+	 * Trouve aléatoirement un héros sur la carte parmi les cases adjacentes (8) à {@code pos}.
+	 * @param pos la position à partir de laquelle on recherche un héros 
+	 * @return un héros
+	 */
+	Heros trouveHeros(Position pos);
+	
+	/* ????????????????
+	 * Ajout d'une méthode pour trouver un monstre ? Un obstacle ?
+	 *??????????????*/
+	
+	/**
+	 * Déplace le soldat {@code soldat} à la position {@code pos}.
+	 * @param pos la position à laquelle déplacer le soldat
+	 * @param soldat le soldat à déplacer
+	 * @return {@code true} si le soldat a pu être déplacé, {@code faux} sinon.
+	 */
+>>>>>>> branch 'develop' of https://github.com/Gronahak/ProjetBDT.git
 	boolean deplaceSoldat(Position pos, Soldat soldat);
+	
+	/**
+	 * Tue le soldat en paramètre.
+	 * @param perso le soldat à tuer
+	 */
 	void mort(Soldat perso);
+	
+	/**
+	 * Vérifie si/Réalise ? l'action du héros est possible d'une première position à une seconde position
+	 * @param pos première position de 'action
+	 * @param pos2 seconde postion de l'action
+	 * @return {@code true} si l'action est réalisée/peut être réalisée, {@code false} sinon
+	 */
 	boolean actionHeros(Position pos, Position pos2);
+	
+	/**
+	 * Fais jouer l'ensemble des soldats
+	 * @param pj le panneau de jeu
+	 */
 	void jouerSoldats(PanneauJeu pj);
+	
+	/**
+	 * Dessine l'ensemble de la carte
+	 * @param g le graphics à dessiner
+	 */
 	void toutDessiner(Graphics g); 
 }
--- Projet_BDT/src/wargame/Obstacle.java
+++ Projet_BDT/src/wargame/Obstacle.java
@@ -1,21 +1,69 @@
 package wargame;
 import java.awt.Color;
+<<<<<<< HEAD
+=======
+import java.awt.Graphics;
+
+/**
+ * Classe définissant les obsatcles du jeu. Hérite de {@linkplain Element} et implémente {@linkplain IConfig}.
+ * @author Rémy
+ * @version 16/11/2016
+ * @see IConfig Element
+ */
+>>>>>>> branch 'develop' of https://github.com/Gronahak/ProjetBDT.git
 public class Obstacle extends Element implements IConfig{
+	/**
+	 * Définit le type possible d'un obstacle.
+	 * @author Rémy
+	 * @version 16/11/2016
+	 */
 	public enum TypeObstacle {
-		ROCHER (COULEUR_ROCHER), FORET (COULEUR_FORET), EAU (COULEUR_EAU);
-		private final Color COULEUR;
-		TypeObstacle(Color couleur) { COULEUR = couleur; }
+		/** Obstacle de type rocher */
+		ROCHER (COULEUR_ROCHER), /** Obstacle de type forêt */FORET (COULEUR_FORET), /** Obstacle de type eau */EAU (COULEUR_EAU);
+		private final Color COULEUR; // couleur de l'élément
+		/**
+		 * Construit un nouveau TypeObstacle
+		 * @param couleur la couleur donné à l'obsatcle (correspondant à son type)
+		 */
+		TypeObstacle(Color couleur) { 
+			COULEUR = couleur; 
+		}
+		/**
+		 * Définit un TypeObstacle de manière aléatoire.
+		 * @return un nouveau TypeObstacle aléatoire
+		 */
 		public static TypeObstacle getObstacleAlea() {
 			return values()[(int)(Math.random()*values().length)];
 		}
 	}
 	
+<<<<<<< HEAD
+=======
+//	Type dl'obstacle (obtenu avec enum)
+>>>>>>> branch 'develop' of https://github.com/Gronahak/ProjetBDT.git
 	private TypeObstacle TYPE;
 	
+<<<<<<< HEAD
+=======
+	/**
+	 * Construit un nouvel obstacle.
+	 * @param type le type de l'obstacle à créer
+	 * @param pos la position de l'obstacle à créer
+	 */
+>>>>>>> branch 'develop' of https://github.com/Gronahak/ProjetBDT.git
 	Obstacle(TypeObstacle type, Position pos) { 
+<<<<<<< HEAD
 		TYPE = type; this.pos = pos;
 		couleur=TYPE.COULEUR;
+=======
+		super(pos, type.COULEUR);
+		TYPE = type; this.pos = pos; 
+>>>>>>> branch 'develop' of https://github.com/Gronahak/ProjetBDT.git
 	}
+	
+	/**
+	 * Affichage pour un obstacle
+	 */
 	public String toString() {
 		return ""+TYPE;
 	}
