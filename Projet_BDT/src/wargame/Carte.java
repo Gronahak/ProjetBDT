265d0379505d0303a78fabf9ed194cbf050eb532
Remy <remyduverger95@gmail.com>
2016-11-16 18:38:39 +0100

    Ajout de la javadoc aux différentes classes de bases

--- Projet_BDT/src/wargame/Carte.java
+++ Projet_BDT/src/wargame/Carte.java
@@ -1,19 +1,26 @@
 package wargame;
 
 import java.awt.Graphics;
-
+/**
+ * Classe gérant la carte du jeu et les éléments présents sur celle-ci.
+ * @author Hugo
+ * @version 16/11/2016
+ * @see IConfig ICarte
+ */
 public class Carte implements ICarte,IConfig{
+	/** Carte du jeu : Matrice d'{@linkplain Element} */ 
 	private Element[][] elements = new Element[LARGEUR_CARTE][HAUTEUR_CARTE];
 	
-	Element getElement(Position pos){
+	public Element getElement(Position pos){
 		return elements[pos.getX()][pos.getY()];
 	}
-	Position trouvePositionVide(){ 				// Trouve aléatoirement une position vide sur la carte
-		
+	
+	public Position trouvePositionVide(){
+		return new Position(0, 0); //position nulle 
 	}
-	Position trouvePositionVide(Position pos); 	// Trouve une position vide choisie aléatoirement parmi les 8 positions adjacentes de pos
-	Heros trouveHeros(); 						// Trouve aléatoirement un héros sur la carte
-	Heros trouveHeros(Position pos); 			// Trouve un héros choisi aléatoirement parmi les 8 positions adjacentes de pos
+	Position trouvePositionVide(Position pos); 	
+	Heros trouveHeros(); 						
+	Heros trouveHeros(Position pos); 
 	boolean deplaceSoldat(Position pos, Soldat soldat);
 	void mort(Soldat perso);
 	boolean actionHeros(Position pos, Position pos2);
--- Projet_BDT/src/wargame/IConfig.java
+++ Projet_BDT/src/wargame/IConfig.java
@@ -1,17 +1,43 @@
 package wargame;
 import java.awt.Color;
 
+/**
+ * Interface définissant les configurations du jeu
+ * @author Rémy
+ * @version 16/11/2016
+ */
 public interface IConfig {
-	int LARGEUR_CARTE = 25; 
-	int HAUTEUR_CARTE = 15; // en nombre de cases
+//	Taille de carte
+	/** Largeur maximale de la carte de jeu (en nombre de cases) */
+	int LARGEUR_CARTE = 25;
+	/** Hauteur maximale de la carte de jeu (en nombres de cases) */
+	int HAUTEUR_CARTE = 15;
+	
+//	Pixels par case
+	/** Nombre de pixels par case de jeu (case carrée) */
 	int NB_PIX_CASE = 20;
+	
+//	Position de la fenêtre de jeu
+	/** Position de la fenêtre de jeu (en x) */
 	int POSITION_X = 100; 
-	int POSITION_Y = 50; // Position de la fenêtre
+	/** Position de la fenêtre de jeu (en y) */
+	int POSITION_Y = 50;
+	
+//	Nombres d'éléments du jeu
+	/** Nombre de héros au début du jeu */
 	int NB_HEROS = 6; 
+	/** Nombre de monstres au début du jeu */
 	int NB_MONSTRES = 15; 
+	/** Nombre d'obstacles au début du jeu */
 	int NB_OBSTACLES = 20;
-	Color COULEUR_VIDE = Color.white, COULEUR_INCONNU = Color.lightGray;
-	Color COULEUR_TEXTE = Color.black, COULEUR_MONSTRES = Color.black;
-	Color COULEUR_HEROS = Color.red, COULEUR_HEROS_DEJA_JOUE = Color.pink;
-	Color COULEUR_EAU = Color.blue, COULEUR_FORET = Color.green, COULEUR_ROCHER = Color.gray;
+	
+//	Couleurs de base des cases
+	/** Couleur d'une case sans élément */
+	Color COULEUR_VIDE = Color.white, /** Couleur d'une case inconnue */COULEUR_INCONNU = Color.lightGray;
+	/** Couleur du texte */
+	Color COULEUR_TEXTE = Color.black;
+	/** Couleur des monstres */
+	Color COULEUR_MONSTRES = Color.black, /** Couleur d'un héros */COULEUR_HEROS = Color.red, /** Couleur d'un héros ayant déjà joué pour ce tour */COULEUR_HEROS_DEJA_JOUE = Color.pink;
+	/** Couleur d'un obstacle de type "eau" */
+	Color COULEUR_EAU = Color.blue, /** Couleur d'un obstacle de type "forêt" */COULEUR_FORET = Color.green, /** Couleur d'un obstacle de type "rocher" */COULEUR_ROCHER = Color.gray;
 }
