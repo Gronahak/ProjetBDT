274f9d6d6b353461570d6cb62594d60b933943e0
Remy <remyduverger95@gmail.com>
2016-11-16 12:57:02 +0100

    Ajout de la classe Element (commit de test)

---
+++ Projet_BDT/src/wargame/Element.java
@@ -0,0 +1,11 @@
+package wargame;
+/**
+ * Classe définissant les élements de la carte du jeu.
+ * @author Rémy
+ */
+public class Element implements IConfig{
+	/** Construit un nouvel Element */
+	public Element(){
+		
+	}
+}
