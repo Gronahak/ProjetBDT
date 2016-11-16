30de458d2223cf226f24c156c1a06fc136e1595d
hugo <hugo@francis.home>
2016-11-16 21:51:10 +0100

    Premiere version du panneau de jeu (Ya encore du taf aussi)

--- Projet_BDT/src/wargame/PanneauJeu.java
+++ Projet_BDT/src/wargame/PanneauJeu.java
@@ -2,6 +2,26 @@ package wargame;
 import java.awt.*;
 import javax.swing.*;
 
-public class PanneauJeu extends JPanel {
+public class PanneauJeu extends JPanel implements IConfig {
 
+	
+	private static final long serialVersionUID = 1L;
+	Carte testcarte=new Carte();
+
+	PanneauJeu(){
+	//	this.setOpaque(true);
+	//	this.setBackground(Color.GRAY); 
+		this.setPreferredSize(new Dimension(LARGEUR_CARTE*NB_PIX_CASE,HAUTEUR_CARTE*NB_PIX_CASE));
+
+		
+	//	this.add(testcarte);
+	//testcarte.toutDessiner(this.getGraphics());
+
+	}
+	public void paintComponent(Graphics g){
+
+		super.paintComponent(g);
+		testcarte.toutDessiner(g);
+
+	}
 }
