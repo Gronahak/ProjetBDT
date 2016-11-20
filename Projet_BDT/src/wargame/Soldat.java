eaac2475a96f745aad902dcb0d34e314a9b2db23
hugo <hugo@francis.home>
2016-11-20 20:59:01 +0100

    Soldat : ajouts de méthodes
    	estAPortee: dit si un soldat ennemi est à portée de tir
    	diminuerPointsVieActuels : diminue les pvs actuels d'un montant passé
    en paramètre
    	getter des pvs actuels
    	implémentation de seDeplace
    	implémentation de combat

--- Projet_BDT/src/wargame/Soldat.java
+++ Projet_BDT/src/wargame/Soldat.java
@@ -25,6 +25,7 @@ public  class Soldat  extends Element implements ISoldat  {
 		puissance = type.getPuissance();
 		this.nom=nom;
 		this.race=type.name();
+		setCarte(carte);
 	}
 	public Soldat(Carte carte,TypesH type,String nom,Position pos){
 		super(pos);
@@ -47,15 +48,45 @@ public  class Soldat  extends Element implements ISoldat  {
 	}
 	
 	public void combat(Soldat soldat){
+		int forceDeFrappe;
+		if (this.pos.estVoisine(soldat.pos)){
+			forceDeFrappe=(int)Math.floor(Math.random()*this.getPuissance());
+			soldat.diminuerPointsVieActuels(forceDeFrappe);
+			if (soldat.getPointsVieActuels()<=0) carte.mort(soldat);
+			else {
+				forceDeFrappe=(int)Math.floor(Math.random()*soldat.getPuissance());
+				this.diminuerPointsVieActuels(forceDeFrappe);
+				if (this.getPointsVieActuels()<=0) carte.mort(this);
+			}
+		}
+		else {
+			forceDeFrappe=(int)Math.floor(Math.random()*this.getTir());
+			soldat.diminuerPointsVieActuels(forceDeFrappe);
+			if (soldat.getPointsVieActuels()<=0) carte.mort(soldat);
+			else {
+				if (soldat.estAPortee(this)){
+					forceDeFrappe=(int)Math.floor(Math.random()*soldat.getPuissance());
+					this.diminuerPointsVieActuels(forceDeFrappe);
+					if (this.getPointsVieActuels()<=0) carte.mort(this);
+				}
+			}
+		}
 		
 	}
 
 	public void seDeplace(Position newPos){
+		if (newPos.estValide()&&carte.estVide(newPos)) {
+			this.setPosition(newPos);
 		
+			System.out.println("Soldat seDeplace" + pos);
+		}
 	}
 
 	public int getPoints() {
 		return pointsVieMax;
+	}
+	public int getPointsVieActuels() {
+		return pointsVieActuels;
 	}
 
 	public int getPuissance() {
@@ -69,7 +100,15 @@ public  class Soldat  extends Element implements ISoldat  {
 	public int getPortee() {
 		return portee;
 	}
+	public void diminuerPointsVieActuels(int pv){
+		pointsVieActuels-=pv;
+	}
 	public String toString(){
 		return " "+race+" "+nom+" (" + pointsVieActuels + "PV/" + pointsVieMax + ")";
+	}
+	
+	public Boolean estAPortee(Soldat ennemi){
+		if (this.pos.distanceX(ennemi.pos)<=this.getPortee()&&this.pos.distanceY(ennemi.pos)<=this.getPortee()) return true;
+		return false;
 	}
 }
