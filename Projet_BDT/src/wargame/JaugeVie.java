4e57f11cb0bc5af879954e54b072fbac53a7514c
hugo <hugo@francis.home>
2016-12-11 10:12:35 +0100

    gestion des jauges de vie

---
+++ Projet_BDT/src/wargame/JaugeVie.java
@@ -0,0 +1,54 @@
+package wargame;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+public class JaugeVie extends JPanel implements IConfig{
+
+	private int hpTotaux;//=1; /* Points de vie totaux*/
+	private int hpCourants;//=1;
+	JaugeVie(){
+		super();
+		this.setBackground(Color.DARK_GRAY);
+		this.setPreferredSize(new Dimension( 3* NB_PIX_CASE, HAUTEUR_CARTE * NB_PIX_CASE));
+		//this.setOpaque(false);
+		 setBorder (new LineBorder(Color.black)); 
+		// Graphics g=getGraphics();
+	}
+	
+	public void setHp(int hp){ /* Initialiser les pv */
+		hpTotaux=hp;
+		hpCourants=hp;
+	}
+	public void decHp(int dHp){ /* Réduire les pv */
+		System.out.println("ça baisse de "+dHp);
+		hpCourants-=dHp;
+	}
+	public void addHp(int dHp){ /* Augmenter les pv */
+		hpCourants+=dHp;
+		
+	}
+	
+	public void setHpCourants (int hp){
+		hpCourants=hp;
+		System.out.println("Je set les hp à "+hp);
+	
+	}
+
+	
+	public void paintComponent(Graphics g){
+
+		super.paintComponent(g);
+	//
+	//	FenetreJeu.vieArmeeMonstres.seDessiner(g);/g=getGraphics();
+g.setColor(Color.red);
+
+		g.fillRect( NB_PIX_CASE/2, (int)(HAUTEUR_CARTE*NB_PIX_CASE*(float)(hpTotaux-hpCourants)/hpTotaux), NB_PIX_CASE,(int) (HAUTEUR_CARTE*NB_PIX_CASE*(1-((float)(hpTotaux-hpCourants)/hpTotaux))));
+//		g.fill
+		System.out.println("oooooooooook hpcour "+hpCourants+" tot : "+hpTotaux+"bousin "+ ((float)(hpTotaux-hpCourants)/hpTotaux)+"\t"+HAUTEUR_CARTE*NB_PIX_CASE*(1-((float)(hpTotaux-hpCourants)/hpTotaux))+"\t" +(int)(NB_PIX_CASE*HAUTEUR_CARTE*(float)(hpTotaux-hpCourants)/hpTotaux*100));
+	}
+}
